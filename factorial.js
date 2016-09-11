2b5d9b992f4b5e473eeff9aeb1be645bdc43ab35
Vikas <vikaspotluri123@gmail.com>
2016-09-11 11:01:45 -0500

    Add Algorithmic Thinking:Factorial

---
+++ factorial.js
@@ -0,0 +1,18 @@
+function factorial(upper)
+{
+	var factorial = 1, index = 0;
+	function factorial_helper()
+	{
+		index = index+1;
+		factorial = factorial * index;
+	}
+	factorial_helper();
+	while(!(index >= upper))
+		factorial_helper();
+	console.log(upper,factorial);
+}
+
+for(var i = 0; i<100;i++)
+{
+	factorial(i);
+}
